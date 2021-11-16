1ebad22a2b26e8f3723f53bf2b41d767e2cfed30
c0het <c0het401@gmail.com>
2021-11-16 17:29:00 +0300

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,11 +4,9 @@ public class DummyBot {
 
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
-        if ("Привет, Бот.".equals(question)) {
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
-            rsl = "Привет, умник";
-        } else if (question.equals("Пока.")) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+        if (question.equals("Привет, Бот.")) {
+            rsl = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
             rsl = "До скорой встречи.";
         }
         return rsl;
